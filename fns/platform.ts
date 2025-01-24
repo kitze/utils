10cbfe113f0bf54da9e9982e9568f510b077cdf7
Kitze <kristijan.mkd@gmail.com>
2025-01-24 21:32:29 +0100

    YOLO commit

--- fns/platform.ts
+++ fns/platform.ts
@@ -1,4 +1,4 @@
-export const platform = navigator.platform.toLowerCase();
+export const platform: string = navigator.platform.toLowerCase();
 export const isMac: boolean = platform.includes("mac");
 export const isWindows: boolean = platform.includes("win");
 export const isLinux: boolean = platform.includes("linux");
