ba875ec7920dbe6b26097c78d3ca959fdc42efe4
Kitze <kristijan.mkd@gmail.com>
2025-01-25 12:33:49 +0100

    YOLO commit

--- readme.md
+++ readme.md
@@ -1,7 +1,2 @@
 - for dates: date-fns
 - for utils: lodash
-
-### styling
-
-- for tailwind components: twc
-- using v/h or vertical/horizontal with center/center-v/center-h instead of flex, flex-col etc.
--- twc.ts
+++
@@ -1,6 +0,0 @@
-import { twc } from "react-twc";
-import { ReactFC } from "./utils/types";
-
-type TWCProps = { className?: string };
-
-export const Card: ReactFC<TWCProps> = twc.div`rounded-lg border bg-slate-100 text-white shadow-sm`;
